e2e07ef9b0f80e028f448db3dba0422f1ae80c25
James0251 <den4643025@mail.ru>
2020-04-07 02:39:00 +0300

    Соединение с базой данных установлено

--- connect.php
+++ connect.php
@@ -1,1 +1,9 @@
 <?php
+$mysqli = @new mysqli('localhost', 'root', '0000', 'feedback_form');
+if ($mysqli) {
+    echo 'MySQL was connected';
+}else{
+    echo 'MySQL was not connected';
+}
+$mysqli->close();
+?>
