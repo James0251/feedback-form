aa8cb8cd2c9344f517b43d2536f6931623ed313c
James0251 <den4643025@mail.ru>
2020-04-07 09:53:00 +0300

    Создал таблицу в БД

--- connect.php
+++ connect.php
@@ -1,9 +1,30 @@
 <?php
 $mysqli = @new mysqli('localhost', 'root', '0000', 'feedback_form');
-if ($mysqli) {
-    echo 'MySQL was connected';
+//if ($mysqli) {
+//    echo 'MySQL was connected';
+//}else{
+//    echo 'MySQL was not connected';
+//}
+
+//создание таблицы
+$sql = "CREATE TABLE IF NOT EXISTS `feedbackValue` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(255) NOT NULL,
+  `phone` int NOT NULL,
+  `coll` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `specialist` varchar(255) NOT NULL,
+  `service` varchar(255) NOT NULL,
+  `date` DATE NOT NULL,
+  `time` TIME NOT NULL,
+  `creation_date` TIMESTAMP
+)";
+
+if ($mysqli->query($sql) == true){
+    echo "yes";
 }else{
-    echo 'MySQL was not connected';
+    echo "no";
 }
+
 $mysqli->close();
 ?>
